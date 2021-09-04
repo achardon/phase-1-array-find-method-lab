be116e87dab1b7c08d9cf57918bbab30c6cdec64
Alessandra Chardon <achardon@ucla.edu>
2021-09-03 21:38:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 
+
+//the function below passes both tests 
+
+// function superbowlWin(record) {
+//     let winningYear = record.find(element => element.result === "W")
+//     //debugger;
+//     // if (winningYear) {
+//     // return winningYear.year;
+//     // }
+//     // else {
+//     //     return undefined;
+//     // }
+//     return winningYear ? winningYear.year : winningYear;
+// }
+
+
+
+
+
+
+//the function below passes both tests
+let superbowlWin = function (arrayOfObjects) {
+    let winningYear = arrayOfObjects.find((game) => (
+    game.result === "W"
+    ))
+    return winningYear ? winningYear.year : winningYear;
+}
+
+// //this one passes only the second test
+// let superbowlWin = (arrayOfObjects) => arrayOfObjects.find(function(game) 
+// {
+//     let yearWon = game.result === "W";
+//     let yearOnly = game.year;
+//     return yearOnly; 
+// })
